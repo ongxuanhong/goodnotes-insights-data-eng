ee187cd8b957b2b73ff2af047ce6ad7d49cbd8c7
Hong Ong <Hong Ong>
2024-12-18 21:31:27 +0700

    Data profiling using ydata-profiling

--- 02.data_profiling.py
+++ 02.data_profiling.py
@@ -1,19 +1,13 @@
+import pandas as pd
 from ydata_profiling import ProfileReport
-from pyspark.sql import SparkSession
-
-spark = (
-    SparkSession.builder().master("local[1]").appName("SparkProfiling").getOrCreate()
-)
 
 
-def profiling_spark_df(spark, filename):
-    df = spark.read.csv(f"{filename}.csv")
+def profiling_data(filename):
+    pd_data = pd.read_csv(f"data/{filename}.csv")
 
-    df.printSchema()
-
-    a = ProfileReport(df, title=f"Profiling Report: {filename}")
+    a = ProfileReport(pd_data, title=f"Profiling Report: {filename}")
     a.to_file(f"profiles/{filename}.html")
 
 
-profiling_spark_df(spark, filename="user_metadata_sample")
-profiling_spark_df(spark, filename="user_interactions_sample")
+profiling_data(filename="user_metadata_sample")
+profiling_data(filename="user_interactions_sample")
